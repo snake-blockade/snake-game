37baa4d99f5cb43635c22fd7479573379e214b31
NatsuTwin <icemerpro@gmail.com>
2024-05-26 16:45:57 +0200

    fix: IndexOutOfBounds if no arguments were specified. 🚧

--- src/main/java/io/zentae/game/SnakeApplication.java
+++ src/main/java/io/zentae/game/SnakeApplication.java
@@ -5,7 +5,15 @@ import io.zentae.game.loader.GameLoader;
 public class SnakeApplication {
 
     public static void main(String[] args) {
-        new GameLoader().load(args[0], args[1]);
+        // if no channel id has been specified.
+        if(args == null || args.length == 0)
+            new GameLoader().load("ChatTest", "ChatTest");
+        // if only one of the two channel ids has been specified, we assume that the writer channel's id has been told.
+        if(args.length == 1)
+            new GameLoader().load(args[0], "ChatTest");
+        // if both of the channels id have been specified.
+        if(args.length >= 2)
+            new GameLoader().load(args[0], args[1]);
     }
 
 }
