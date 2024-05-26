e82d9fa304b00cf587281778f1f036bd9892c309
NatsuTwin <icemerpro@gmail.com>
2024-05-26 14:32:22 +0200

    fix: bad channels identifier. 🔧

--- src/main/java/io/zentae/game/SnakeApplication.java
+++ src/main/java/io/zentae/game/SnakeApplication.java
@@ -5,7 +5,7 @@ import io.zentae.game.loader.GameLoader;
 public class SnakeApplication {
 
     public static void main(String[] args) {
-        new GameLoader().load("ChatTest", "ChatTest");
+        new GameLoader().load(args[0], args[1]);
     }
 
 }
