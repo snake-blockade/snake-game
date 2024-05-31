0d029abefff2d47504909580687b96b5f07b8226
Yanis Tahiri <yanistahiri560@gmail.com>
2024-05-31 02:18:59 +0200

    refactor: 🎁use of early return.

--- src/main/java/io/zentae/game/SnakeApplication.java
+++ src/main/java/io/zentae/game/SnakeApplication.java
@@ -6,14 +6,17 @@ public class SnakeApplication {
 
     public static void main(String[] args) {
         // if no channel id has been specified.
-        if(args == null || args.length == 0)
+        if(args == null || args.length == 0) {
             new GameLoader().load("ChatTest", "ChatTest");
+            return;
+        }
         // if only one of the two channel ids has been specified, we assume that the writer channel's id has been told.
-        if(args.length == 1)
+        if(args.length == 1) {
             new GameLoader().load(args[0], "ChatTest");
+            return;
+        }
         // if both of the channels id have been specified.
-        if(args.length >= 2)
-            new GameLoader().load(args[0], args[1]);
+        new GameLoader().load(args[0], args[1]);
     }
 
 }
